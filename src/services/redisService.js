ca2eaac95794c183d485ba67fa98199cbd592335
Ayoub <Fanaoui.25@gmail.com>
2025-01-10 23:37:03 +0100

    Ajout de la fonction cacheData pour la gestion du cache Redis

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -3,11 +3,28 @@
 // Question: Quelles sont les bonnes pratiques pour les clés Redis ?
 // Réponse :
 
+const redis = require('redis');
+const client = redis.createClient({ url: process.env.REDIS_URI });
+
+client.on('error', (err) => {
+  console.log('Redis Client Error', err);
+});
+
 // Fonctions utilitaires pour Redis
-async function cacheData(key, data, ttl) {
+async function cacheData(key, data, ttl = 3600) {
     // TODO: Implémenter une fonction générique de cache
-  }
+    return new Promise((resolve, reject) => {
+      client.setex(key, ttl, JSON.stringify(data), (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });  
+}
   
   module.exports = {
     // TODO: Exporter les fonctions utilitaires
+    cacheData
   };
